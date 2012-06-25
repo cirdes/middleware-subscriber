57ee5ebc3d97dd56add548200db9620f37f1f7f5
Cirdes Henrique <cirdes@eventick.com.br>
2012-06-24 22:35:17 -0300

    subscribe

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,13 @@ class Client < ActiveRecord::Base
   attr_accessible :areas, :name
 
   has_many :tags
+
+  after_save :subscribe
+
+  def subscribe
+    url_server = "http://middleware-rfid.herokuapp.com/clients"
+    url_callback = "http://middleware-subscriber.herokuapp.com" + Rails.application.routes.url_helpers.client_tags_path
+    options = { :query => {name: self.name, area: self.areas, url: url_callback }}
+    response = HTTParty.post(url_server, options)
+  end
 end
